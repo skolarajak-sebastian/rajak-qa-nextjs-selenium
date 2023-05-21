b1ed76ae0590b826692b6235a573e257a11c621c
Школа Рајак Себ <skolarajak.sebastian@gmail.com>
2023-05-21 16:18:25 +0200

    return 400 on post

--- pages/api/test-json.js
+++ pages/api/test-json.js
@@ -1,11 +1,16 @@
 
 export default (req, res) => {
 
-    const odgovor = {
-            bitnaInformacija: 'Ovo je bitna informacija',
-            id: 1,
-            queryParam: req.query,
+    const rezultat = 
+    {
+        root: {
+            odgovor: {
+                bitnaInformacija: 'Ovo je bitna informacija',
+                id: 1,
+                queryParam: req.query,
+            }
+        }
     };
 
-    return res.status(200).json(odgovor);
+    return res.status(200).json(rezultat);
 };
